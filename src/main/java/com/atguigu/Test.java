04c1cff04b19e89010de01bcb523b0719562014e
NUC1913041309/NUC1913041309 <1347947403@qq.com>
2023-08-22 18:22:39 +0800

    创建1

---
+++ src/main/java/com/atguigu/Test.java
@@ -0,0 +1,11 @@
+package com.atguigu;
+
+public class Test {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+    }
+}
